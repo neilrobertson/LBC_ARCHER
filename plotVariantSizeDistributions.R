a02991e41e2cd0769b0d32bfd43ac5bb75261b7a
neilrobertson <neil.alistair.robertson@hotmail.co.uk>
2020-04-07 21:43:18 +0100

    update plot distributions

--- plotVariantSizeDistributions.R
+++ plotVariantSizeDistributions.R
@@ -16,7 +16,7 @@ colnames(data)[33] <- "AUAO" # Non unique column field (UAO) - taken from ARCHER
 dim(data)
 
 setwd("/mnt/tchandra-lab/Neil/data/DNA-seq/LBC_ARCHER_Panel-seq/analysis/CloneSizeDistributions")
-
+variant <- "RUNX1 c.1265_1266delinsCC"
 for (variant in (unique(data$key))) {
 	print (variant)
 	variant.data <- subset(data, key == variant)
@@ -32,6 +32,8 @@ for (variant in (unique(data$key))) {
 	p <- p + ylab("Variant Allele Fraction")
 	p <- p + xlab("Wave")
 	p <- p + ggtitle(variant)
+	p <- p + scale_x_continuous(name="Wave",breaks=c(1,2,3,4), labels = c("1","2","3","4"))
+	p <- p + scale_y_continuous(name="Variant Allele Fraction",limits=c(0,0.55),breaks=c(0,.1,.2,.3,.4,.5))
 	p <- p + theme(axis.text.x = element_text(color="black", size=12),
 					axis.text.y = element_text(color="black", size=12))
 	p <- p + theme(axis.text=element_text(size=14),
@@ -44,12 +46,13 @@ for (variant in (unique(data$key))) {
 	png(output_filename, width = 1100, height = 1000, res = 200, pointsize =5)
 	p <- ggplot(data=variant.data, aes(x=participant_id, y=AF, group = wave))
 	p <- p + geom_line(data = variant.data, aes(x=participant_id, y=AF, group = wave, col = as.factor(wave)), size = 1)
-	p <- p + geom_point(data = variant.data, aes(x=participant_id, y=AF), colour = "grey")
+	p <- p + geom_point(data = variant.data, aes(x=participant_id, y=AF, col = as.factor(wave)))
 	p <- p + theme_minimal() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
 									panel.background = element_blank(), axis.line = element_line(colour = "black"))
 	p <- p + ylab("Variant Allele Fraction")
 	p <- p + xlab("Particicipant")
 	p <- p + ggtitle(variant)
+	p <- p + scale_y_continuous(name="Variant Allele Fraction",limits=c(0,0.55),breaks=c(0,.1,.2,.3,.4,.5))
 	p <- p + theme(legend.title=element_blank(), legend.position="bottom")
 	p <- p + theme(axis.text.x = element_text(color="black", size=12, angle = 45, hjust = 1),
 					axis.text.y = element_text(color="black", size=12))
